5fe24ae92cc1fb50ab07704ca4a0dcf4677f4eb5
aussieklutz <aussieklutz@gmail.com>
2021-02-22 07:00:41 +1000

    First cut

--- auto_sudoko.php
+++ auto_sudoko.php
@@ -32,8 +32,9 @@ function display($populate, $override = false) {
     global $sudoku_string;
     $notfound = $totalcells;
     $error = false;
+    if($override) echo PHP_EOL;
     if($GLOBALS['debug'] || $override) echo 'Display' . PHP_EOL;
-    if($populate) if($GLOBALS['debug']) echo 'Populating' . PHP_EOL;
+    if($populate) if($GLOBALS['debug'] || $override) echo 'Populating' . PHP_EOL;
     if($populate) {
         $cells = array();
         $meta = array();
@@ -51,20 +52,20 @@ function display($populate, $override = false) {
                 if($cell_string == '.') {
                     $cells[$i][$j] = array_values($fullset);
                     $meta[$i][$j] = array();
-                    $meta[$i][$j]['setter'] = '';
+                    $meta[$i][$j]['setter'] = '_';
                 } else {
                     $cells[$i][$j] = array($cell_string);
                     $meta[$i][$j] = array();
                     $meta[$i][$j]['setter'] = 'p';
                 }
             }
-            if(count($cells[$i][$j]) == 1 || $override) {
-                if($GLOBALS['debug'] || $override) echo array_values($cells[$i][$j])[0];
+            if(count($cells[$i][$j]) == 1) {
+                if($GLOBALS['debug'] || $override || $populate) echo array_values($cells[$i][$j])[0];
                 $notfound--;
             } elseif (count($cells[$i][$j]) > 1 && count($cells[$i][$j]) <= $rows) {
-                if($GLOBALS['debug'] || $override) echo '.';
+                if($GLOBALS['debug'] || $override || $populate) echo '.';
             } else {
-                if($GLOBALS['debug'] || $override) echo '!';
+                if($GLOBALS['debug'] || $override || $populate) echo '!';
                 $error = true;
             }
             if($GLOBALS['debug']) echo('(' . count($cells[$i][$j]) . ')');
@@ -99,7 +100,7 @@ function reduce() {
             $possible_values = count($cells[$i][$j]);
             if($debug) if($GLOBALS['debug']) echo '(' . $i . ', ' . $j . '): ' . PHP_EOL;
             if($debug) print_r($cells[$i][$j]);
-            if(count($cells[$i][$j]) == 1) {
+            if($possible_values == 1) {
                 // echo 'propogating' . PHP_EOL;
                 // reduce row adjacent values
                 for($k = 0; $k < $cols; $k++) {
@@ -109,8 +110,8 @@ function reduce() {
                         $backup = $cells[$i][$k];
                         $cells[$i][$k] = array_values(array_diff($cells[$i][$k], $cells[$i][$j]));
                         if(count($cells[$i][$k]) == 1) $meta[$i][$k]['setter'] = '-';
-                        if(count($cells[$i][$k]) < 1) $cells[$i][$k] = $backup;
-                        if(count($cells[$i][$k]) < $init) $reduced++;
+                        if(count($cells[$i][$k]) < 1 || verify(false) > 0) $cells[$i][$k] = $backup;
+                        if(count($cells[$i][$k]) < $init) $reduced += $init - count($cells[$i][$k]);
                     }
                 }
                 // reduce col adjacent values
@@ -121,27 +122,29 @@ function reduce() {
                         $backup = $cells[$k][$j];
                         $cells[$k][$j] = array_values(array_diff($cells[$k][$j], $cells[$i][$j]));
                         if(count($cells[$k][$j]) == 1) $meta[$k][$j]['setter'] = '-';
-                        if(count($cells[$k][$j]) < 1) $cells[$k][$j] = $backup;
-                        if(count($cells[$k][$j]) < $init) $reduced++;
+                        if(count($cells[$k][$j]) < 1 || verify(false) > 0) $cells[$k][$j] = $backup;
+                        if(count($cells[$k][$j]) < $init) $reduced += $init - count($cells[$k][$j]);
                     }
                 }
                 // reduce division adjacent values
                 $rowdiv = floor($i / $div);
-                $coldiv = floor($i / $div);
-                for($m = $rowdiv; $m < (floor($rows/$div) + $rowdiv); $m++) {
-                    for($n = $coldiv; $n < (floor($cols/$div) + $coldiv); $n++) {
+                $coldiv = floor($j / $div);
+                for($m = ($rowdiv * $div); $m < (($rowdiv+1) * $div); $m++) {
+                    //echo 'DivI' . $i . ':' . ($rowdiv * $div) . ':' . $m . ':' . ((($rowdiv+1) * $div)-1) . PHP_EOL;
+                    for($n = ($coldiv * $div); $n < (($coldiv+1) * $div); $n++) {
+                        //echo 'DivJ' . $j . ':' . ($coldiv * $div) . ':' . $j . ':' . ((($coldiv+1) * $div)-1) . PHP_EOL;
                         // walk cells in division, exept self, and remove options
                         if($i != $m && $j != $n && count($cells[$m][$n]) > 1) {
                             $init = count($cells[$m][$n]);
                             $backup = $cells[$m][$n];
                             $cells[$m][$n] = array_values(array_diff($cells[$m][$n], $cells[$i][$j]));
                             if(count($cells[$m][$n]) == 1) $meta[$m][$n]['setter'] = '-';
-                            if(count($cells[$m][$n]) < 1) $cells[$m][$n] = $backup;
-                            if(count($cells[$m][$n]) < $init) $reduced++;
+                            if(count($cells[$m][$n]) < 1 || verify(false) > 0) $cells[$m][$n] = $backup;
+                            if(count($cells[$m][$n]) < $init) $reduced += $init - count($cells[$m][$n]);
                         }
                     }
                 }
-                // reduce diagonal adjacent values
+                /*// reduce diagonal adjacent values
                 if($i == $j || ($rows - $i) == $j) {
                     // reduce col adjacent values
                     for($k = 0; $k < $rows; $k++) {
@@ -151,17 +154,16 @@ function reduce() {
                             $backup = $cells[$i][$k];
                             $cells[$k][$k] = array_values(array_diff($cells[$k][$k], $cells[$i][$j]));
                             if(count($cells[$k][$k]) == 1) $meta[$k][$k]['setter'] = '-';
-                            if(count($cells[$k][$k]) < 1) $cells[$k][$k] = $backup;
-                            if(count($cells[$k][$k]) < $init) $reduced++;
+                            if(count($cells[$k][$k]) < 1 || verify(false) > 0) $cells[$k][$k] = $backup;
+                            if(count($cells[$k][$k]) < $init) $reduced += $init - count($cells[$m][$n]);
                         }
                     }
-                }
+                }*/
             }
-            $cells[$i][$j] = array_values($cells[$i][$j]);
-            if(count($cells[$i][$j]) == 1) $meta[$i][$j]['setter'] = '-';
             if($debug) sleep(1);
         }
     }
+    echo '-';
     return $reduced;
 }
 
@@ -181,16 +183,24 @@ function rando($randoffset) {
         if($GLOBALS['debug']) echo PHP_EOL . 'Random Cell Found: (' . $rand_row . ', ' . $rand_col . '): ' . count($cells[$rand_row][$rand_col]) . PHP_EOL;
         if($GLOBALS['debug']) echo 'Existing Values: ' . implode(',', $cells[$rand_row][$rand_col]) . PHP_EOL;
         $attempt_value = array_values($cells[$rand_row][$rand_col])[rand(0, count($cells[$rand_row][$rand_col])-1)];
+        $backup_value = $cells[$rand_row][$rand_col];
+        $backup_meta = $meta[$rand_row][$rand_col];
         $cells[$rand_row][$rand_col] = array($attempt_value);
         $meta[$rand_row][$rand_col]['setter'] = '?';
-        if($GLOBALS['debug']) echo 'New Value: ' . implode(',', $cells[$rand_row][$rand_col]) . PHP_EOL;
+        if(verify(false) > 0) {
+            $cells[$rand_row][$rand_col] = $backup_value;
+            $meta[$rand_row][$rand_col] = $backup_meta;
+            if($GLOBALS['debug']) echo 'Invalid Value: ' . $attempt_value . PHP_EOL;
+        } else {
+            if($GLOBALS['debug']) echo 'New Value: ' . implode(',', $cells[$rand_row][$rand_col]) . PHP_EOL;
+        }
     } else {
         $randoffset++;
     }
     return $randoffset;
 }
 
-function verify() {
+function verify($override = true) {
     global $rows, $cols, $div, $totalcells, $debug;
     global $cells, $meta;
     $vissues = 0;
@@ -203,47 +213,51 @@ function verify() {
     // walk the cells
     for($i = 0; $i < $rows; $i++) {
         for($j = 0; $j < $cols; $j++) {
-            // reduce row adjacent values
-            for($k = 0; $k < $cols; $k++) {
-                // walk columns in row, exept self, and remove options
-                if($k != $j && $cells[$i][$j][0] == $cells[$i][$k][0]) $hissues++;
-            }
-            // reduce col adjacent values
-            for($k = 0; $k < $rows; $k++) {
-                // walk rows in column, exept self, and remove options
-                if($k != $i && $cells[$i][$j][0] == $cells[$k][$j][0]) $vissues++;
-            }
-            // reduce division adjacent values
-            $rowdiv = floor($i / $div);
-            $coldiv = floor($i / $div);
-            for($m = $rowdiv; $m < (floor($rows/$div) + $rowdiv); $m++) {
-                for($n = $coldiv; $n < (floor($cols/$div) + $coldiv); $n++) {
-                    // walk cells in division, exept self, and remove options
-                    if($i != $m && $j != $n && $cells[$i][$j][0] == $cells[$m][$n][0]) $divissues++;
+            if(count($cells[$i][$j]) == 1) {
+                // reduce row adjacent values
+                for($k = 0; $k < $cols; $k++) {
+                    // walk columns in row, exept self, and remove options
+                    if($k != $j && count($cells[$i][$k]) == 1 && $cells[$i][$j][0] == $cells[$i][$k][0]) $hissues++;
                 }
-            }
-            // reduce diagonal adjacent values
-            if($i == $j || ($rows - $i) == $j) {
                 // reduce col adjacent values
                 for($k = 0; $k < $rows; $k++) {
-                    // walk cels in diagonal, exept self, and remove options
-                    if($k != $i && $k != $j && $cells[$i][$j] == $cells[$k][$k]) $divissues++;
+                    // walk rows in column, exept self, and remove options
+                    if($k != $i && count($cells[$k][$j]) == 1 && $cells[$i][$j][0] == $cells[$k][$j][0]) $vissues++;
                 }
+                // reduce division adjacent values
+                $rowdiv = floor($i / $div);
+                $coldiv = floor($j / $div);
+                for($m = ($rowdiv * $div); $m < (($rowdiv+1) * $div); $m++) {
+                    //echo 'DivI' . $i . ':' . ($rowdiv * $div) . ':' . $m . ':' . ((($rowdiv+1) * $div)-1) . PHP_EOL;
+                    for($n = ($coldiv * $div); $n < (($coldiv+1) * $div); $n++) {
+                        //echo 'DivJ' . $j . ':' . ($coldiv * $div) . ':' . $j . ':' . ((($coldiv+1) * $div)-1) . PHP_EOL;
+                        // walk cells in division, exept self, and remove options
+                        if($i != $m && $j != $n && count($cells[$m][$n]) == 1 && $cells[$i][$j][0] == $cells[$m][$n][0]) $divissues++;
+                    }
+                }/*
+                // reduce diagonal adjacent values
+                if($i == $j || ($rows - $i) == $j) {
+                    // reduce col adjacent values
+                    for($k = 0; $k < $rows; $k++) {
+                        // walk cels in diagonal, exept self, and remove options
+                        if($k != $i && $k != $j && count($cells[$k][$k]) == 1 && $cells[$i][$j] == $cells[$k][$k]) $divissues++;
+                    }
+                }*/
+                if($meta[$i][$j]['setter'] == 'p') $set++;
+                if($meta[$i][$j]['setter'] == '-') $reduce++;
+                if($meta[$i][$j]['setter'] == '?') $rando++;
             }
-            if($meta[$i][$j]['setter'] == 'p') $set++;
-            if($meta[$i][$j]['setter'] == '-') $reduce++;
-            if($meta[$i][$j]['setter'] == '?') $rando++;
         }
     }
-    echo 'Vertical: ' . $vissues . PHP_EOL;
-    echo 'Horizontal: ' . $hissues . PHP_EOL;
-    echo 'Division: ' . $divissues . PHP_EOL;
-    echo 'Diagonal: ' . $diagissues . PHP_EOL;
-    echo 'Total: ' . ($vissues + $hissues + $divissues + $diagissues) . PHP_EOL;
-    echo 'Set: ' . $set . PHP_EOL;
-    echo 'Random: ' . $rando . PHP_EOL;
-    echo 'Reduce: ' . $reduce . PHP_EOL;
-    echo 'Total: ' . ($set + $rando + $reduce) . PHP_EOL;
+    if($override) echo 'Vertical: ' . $vissues . PHP_EOL;
+    if($override) echo 'Horizontal: ' . $hissues . PHP_EOL;
+    if($override) echo 'Division: ' . $divissues . PHP_EOL;
+    if($override) echo 'Diagonal: ' . $diagissues . PHP_EOL;
+    if($override) echo 'Total: ' . ($vissues + $hissues + $divissues + $diagissues) . PHP_EOL;
+    if($override) echo 'Set: ' . $set . PHP_EOL;
+    if($override) echo 'Random: ' . $rando . PHP_EOL;
+    if($override) echo 'Reduce: ' . $reduce . PHP_EOL;
+    if($override) echo 'Total: ' . ($set + $rando + $reduce) . PHP_EOL;
     return $vissues + $hissues + $divissues + $diagissues;
 }
 
@@ -260,31 +274,20 @@ function solve($challenge) {
             echo '================' . PHP_EOL;
             if($GLOBALS['debug']) echo 'Generating Cells' . PHP_EOL;
             $cells = array();
-            display(true);
+            display(true, true);
+            if(verify() > 0) continue;
             $randoffset = 0;
-            $lastrando = false;
-            $randobackup = $cells;
-            $randobackup2 = $meta;
             do {  
-                if(reduce() == 0) {
-                    if(false && $lastrando) {
-                        $cells = $randobackup;
-                        $meta = $randobackup2;
-                        echo '!';
-                    } else {
-                        echo '?';
-                    }
+                $reduce = reduce();
+                $reduce += reduce();
+                if($reduce == 0) {
+                    echo '?';
                     $randoffset = rando($randoffset);
-                    $lastrando = true;
                 } else {
-                    echo '-';
                     if($GLOBALS['debug']) echo 'Reduce Round' . PHP_EOL;
-                    $lastrando = false;
-                    $randobackup = $cells;
-                    $randobackup2 = $meta;
                     $randoffset = 0;
                 }
-            } while (display(false) > 0);
+            } while (display(false, true) > 0);
             echo PHP_EOL;
             file_put_contents('attempt.json', json_encode($cells));
             //sleep(10);
